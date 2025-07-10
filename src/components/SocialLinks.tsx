93f1e3fcc128fb9cf3bc3536bec1317a6d28ac69
King <kingsley.adarkwah@gmail.com>
2025-07-10 23:15:34 +0000

    add null state to social links

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -3,6 +3,10 @@ import { SocialLink } from './index';
 import { socialLinks } from '@/lib/data';
 
 const SocialLinks = () => {
+  // Check if socialLinks is defined and has items
+  if (!socialLinks || socialLinks.length === 0) return null; 
+  
+  // Render social links if available
   return (
     <section>
       {
