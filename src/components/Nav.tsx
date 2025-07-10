30e48df87042ae7d01a178bad76bb9006db15e5b
King <kingsley.adarkwah@gmail.com>
2025-07-10 23:10:49 +0000

    implement nav component

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { navLinks } from '@/lib/data'
+import Link from 'next/link'
+import { NavLink } from '@/types/types';
+
 
 const Nav = () => {
+  // Check if navLinks is defined and has items
+  if (!navLinks || navLinks.length === 0) return null;
+
+  // Render the navigation links
   return (
-    <div>Nav</div>
+    <nav aria-label="Main Navigation" className=''>
+      <ul>
+        {navLinks.map((link: NavLink) => (
+          <li key={link.id}>
+            <Link href={link.href} className='nav-link'>
+              {link.name}
+            </Link> 
+          </li>
+        ))}
+      </ul>
+    </nav>
   )
 }
 
