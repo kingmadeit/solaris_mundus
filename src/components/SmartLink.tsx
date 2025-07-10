9e33a3bcbde8c4d751a90f0e8469194779f2b23f
King <kingsley.adarkwah@gmail.com>
2025-07-10 20:41:15 +0000

    implement SmartLink component

--- src/components/SmartLink.tsx
+++ src/components/SmartLink.tsx
@@ -1,9 +1,39 @@
 import React from 'react'
+import clsx from 'clsx';
+import Link from 'next/link';
 
-const SmartLink = () => {
-  return (
-    <div>SmartLink</div>
-  )
+type SmartLinkProps = {
+    href: string;
+    children: React.ReactNode;
+    target?: '_blank' | '_self';
+    rel?: string;
+    className?: string;
+};
+
+
+const EXTERNAL = ['http', 'https', 'www', '//'];
+
+const SmartLink = ({href, children, target, rel, className}: SmartLinkProps) => {
+
+    const isExternal = EXTERNAL.some(prefix => href.startsWith(prefix));
+
+    if (isExternal) {
+        return (
+            <a 
+                href={href} 
+                target={target || '_blank'} 
+                rel={rel || 'noopener noreferrer'} 
+                className={clsx('', className)}>
+                {children}
+            </a>
+        )
+    }
+
+    return (
+        <Link href={href} className={className}>
+            {children}
+        </Link>
+    )
 }
 
 export default SmartLink
