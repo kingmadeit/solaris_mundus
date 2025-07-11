be64584bab821f94dfff1f245536a6a9e8f7f2b5
King <kingsley.adarkwah@gmail.com>
2025-07-11 06:49:59 +0000

    add arrow-right icon to social link

--- src/components/SocialLink.tsx
+++ src/components/SocialLink.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {SmartLink} from '@/components/index'
 import { TSocialLink } from '@/types/types';
+import {IconArrowRight} from '@tabler/icons-react';
 
 type SocialLinkProps = {
   link: TSocialLink 
@@ -10,8 +11,11 @@ const SocialLink = ({link}: SocialLinkProps) => {
   const { href, icon: Icon, name } = link;
   return (
     <SmartLink className='w-full flex justify-between' href={href} target='_blank' rel='noopener noreferrer'>
-      {Icon && <Icon className='w-6 h-6' />}
-      <span className='label'>{name}</span>
+      <div className='w-90 h-5 flex space-x-2'>
+        {Icon && <Icon className='w-6 h-6' />}
+        <span className='label'>{name}</span>
+      </div>
+      <IconArrowRight className='w-6 h-6'/>
     </SmartLink>
   )
 }
