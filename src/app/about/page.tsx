2b476393df38d83d6695bda25cb5f318541f880d
King <kingsley.adarkwah@gmail.com>
2025-07-11 09:03:58 +0000

    update about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,16 @@
+import React from 'react';
+import Image from 'next/image';
+
 export default function AboutPage() {
     return (
-        <section className="bg-purple-300 w-full flex flex-col items-center justify-start min-h-screen">
-        <h1>About Us</h1>
-        <p>Welcome to the about page</p>
+        <section className="bg-pink-300 w-full flex flex-col items-center justify-start min-h-screen">
+            <Image
+                src="https://images.unsplash.com/photo-1721323960622-124edcae5005?q=80&w=874&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt="About Us"
+                width={800}
+                height={600}
+                className="rounded-lg shadow-md mb-8"
+            />
         </section>
     );
 }
