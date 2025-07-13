d9c8bff160788b7da5795badb9da7f9bb05dea1b
King <kingsley.adarkwah@gmail.com>
2025-07-13 06:01:39 +0000

    clamp section title

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -4,6 +4,10 @@ type SectionTitleProps = {
   title: string;
 };
 
-const SectionTitle = ({title}: SectionTitleProps ) => <h1 className='font-antonio text-6xl font-bold'>{title}</h1>
+const SectionTitle = ({ title }: SectionTitleProps) => (
+  <h1 className="font-antonio font-bold text-[clamp(2rem,10vw,4rem)]">
+    {title}
+  </h1>
+)
 
 export default SectionTitle
