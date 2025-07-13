6dfebff560133f8cea87898106321d036ca1ac26
King <kingsley.adarkwah@gmail.com>
2025-07-13 05:51:08 +0000

    update profle pic

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,13 +3,13 @@ import Image from 'next/image';
 
 export default function AboutPage() {
     return (
-        <section className="bg-pink-300 w-full flex flex-col items-center justify-start min-h-screen">
+        <section className="bg-pink-300 w-full h-screen flex flex-col items-center justify-start min-h-screen">
             <Image
-                src="https://images.unsplash.com/photo-1721323960622-124edcae5005?q=80&w=874&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                src="/images/profile-pic-large.png"
                 alt="About Us"
                 width={800}
                 height={600}
-                className="rounded-lg shadow-md mb-8"
+                className="w-full h-full object-cover"
             />
         </section>
     );
