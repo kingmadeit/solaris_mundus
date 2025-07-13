cd71f90aea90142d8c7669e1f95da4e4ea0aa9dc
King <kingsley.adarkwah@gmail.com>
2025-07-13 05:36:14 +0000

    update footer with copyright

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,12 @@ import React from 'react'
 import { SocialLinks } from '@/components/index'
 const Footer = () => {
   return (
-    <footer className='w-full flex flex-col h-full bg-pink-300'>
+    <footer className='w-full flex flex-col h-full'>
       <SocialLinks />
+      <div className='w-full flex py-4 text-gray-700 text-xs'>
+        <span><small>&copy;</small> {new Date().getUTCFullYear()}</span>
+        <span className='ml-2'>Solaris Mundus LLC.</span>
+      </div>
     </footer>
   )
 }
