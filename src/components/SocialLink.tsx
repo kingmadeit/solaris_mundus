9be13b1f3c2a8defd43442002938f6af07d26be0
King <kingsley.adarkwah@gmail.com>
2025-07-16 00:37:24 +0000

    update social links styling

--- src/components/SocialLink.tsx
+++ src/components/SocialLink.tsx
@@ -10,12 +10,12 @@ type SocialLinkProps = {
 const SocialLink = ({link}: SocialLinkProps) => {
   const { href, icon: Icon, name } = link;
   return (
-    <SmartLink className='w-full flex text-gray-500 py-2 text-xs md:text-[clamp(.75rem, 2vw, 1rem)] justify-between' href={href} target='_blank' rel='noopener noreferrer'>
-      <div className='w-90 h-5 flex space-x-2'>
-        {Icon && <Icon className='w-4 h-4' />}
+    <SmartLink className='w-full flex text-gray-500 py-2 font-normal text-[clamp(1rem, 2vw, 1rem)] justify-between' href={href} target='_blank' rel='noopener noreferrer'>
+      <div className='w-90 h-5 items-center flex space-x-2'>
+        {Icon && <Icon className='w-5 h-5' />}
         <span className='label'>{name}</span>
       </div>
-      <IconArrowRight className='w-6 h-4'/>
+      <IconArrowRight className='w-4 h-4'/>
     </SmartLink>
   )
 }
