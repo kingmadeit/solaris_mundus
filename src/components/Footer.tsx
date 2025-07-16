383564de1d099614a74c249fd673908f4097bcd9
King <kingsley.adarkwah@gmail.com>
2025-07-16 23:40:56 +0000

    animation all footer elements

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,21 @@
+'use client'
 import React from 'react'
 import { SocialLinks } from '@/components/index'
+import * as motion from 'motion/react-client'
+import { fadeInRightAnimation } from '@/lib/constants'
+import { usePageAlias } from '@/hooks'
+
 const Footer = () => {
+  const pageAlias = usePageAlias();
   return (
-    <footer className='w-full flex flex-col h-full'>
+    <motion.footer key={pageAlias} {...fadeInRightAnimation} className='w-full flex flex-col h-full'>
       <SocialLinks />
-      <div className='w-full flex py-4 text-gray-700 text-xs'>
+      
+      <section className='w-full flex py-4 text-gray-700 text-xs'>
         <span><small>&copy;</small> {new Date().getUTCFullYear()}</span>
         <span className='ml-2'>Solaris Mundus LLC.</span>
-      </div>
-    </footer>
+      </section>
+    </motion.footer>
   )
 }
 
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,26 +4,26 @@ export const LAYOUT_PT_MD = 46;
 export const SPACING = 0.25;
 
 export const fadeInRightAnimation = {
-  initial: { opacity: 0, x: DEFAULT_LEFT_VAL },
+  initial: { opacity: 1, x: DEFAULT_LEFT_VAL },
   animate: { opacity: 1, x: 0 },
-  transition: { duration: 0.5 }
+  transition: { duration: 0.5}
 };
 
 export const fadeInLeftAnimation = {
-  initial: { opacity: 0, x: -(DEFAULT_LEFT_VAL * 3) },
+  initial: { opacity: 1, x: -(DEFAULT_LEFT_VAL * 3) },
   animate: { opacity: 1, x: 0 },
   transition: { duration: 0.3}
 };
 
 
 export const fadeInUpAnimation = {
-  initial: { opacity: 0, y: -(DEFAULT_LEFT_VAL * 3) },
+  initial: { opacity: 1, y: -(DEFAULT_LEFT_VAL * 3) },
   animate: { opacity: 1, y: 0 },
   transition: { duration: 0.5 }
 };
 
 export const fadeInDownAnimation = {
-  initial: { opacity: 0, y: -50 },
+  initial: { opacity: 1, y: -50 },
   animate: { opacity: 1, y: 0 },
   transition: { duration: 0.5 }
 };
