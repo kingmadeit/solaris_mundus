f658586baf2068d5d28c8b42e0cf60b72ce43cd2
King <kingsley.adarkwah@gmail.com>
2025-07-17 00:07:49 +0000

    fix contact page overflows

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,14 +1,15 @@
+import { FIXED_ASIDE } from "@/lib/constants";
 import Image from "next/image";
 
 export default function ContactPage() {
     return (
-        <section className="w-full flex flex-col items-center justify-start min-h-screen">
+        <section className={FIXED_ASIDE}>
          <Image
             src="https://images.unsplash.com/photo-1722888898265-130c6d013ceb?q=80&w=928&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            objectFit='cover'
+            layout='fill'
             alt="Contact Us"
-            width={800}
-            height={600}
-            className="rounded-lg shadow-md mb-8"
+            className="w-full"
          />
         </section>
     );
